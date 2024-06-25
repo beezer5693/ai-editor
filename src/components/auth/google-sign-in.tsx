8e5363d782443ca81c066d6a294cf77fc046af3b
Brandon Bryan <bshwag@gmail.com>
2024-06-25 18:47:24 -0400

    added google auth

--- src/components/auth/google-sign-in.tsx
+++ src/components/auth/google-sign-in.tsx
@@ -1,9 +1,27 @@
+"use client";
+
+import { signInWithOAuth } from "@/actions/auth/oauth-signin";
 import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
 
 export default function GoogleSignIn() {
+  const { toast } = useToast();
+
+  const handleSignInWithGoogle = async () => {
+    try {
+      await signInWithOAuth("google");
+    } catch (error: any) {
+      toast({
+        title: "There was a problem!",
+        description: error.message,
+        variant: "destructive",
+      });
+    }
+  };
+
   return (
-    <Button className="w-full gap-2">
+    <Button onClick={handleSignInWithGoogle} className="w-full gap-2">
       <span>
         <Icons.Google />
       </span>
