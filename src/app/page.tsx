26523d4cdb4fd150e122e1c6bbc31682f62fe118
Brandon Bryan <bshwag@gmail.com>
2024-07-15 17:13:31 -0400

    added landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+    return (
+        <main className="flex h-screen flex-col items-center justify-between p-24">
+            <div className="w-full flex h-1/2 flex-col justify-center items-center gap-10">
+                <h1 className="text-6xl lg:text-8xl max-w-screen-lg text-center text-balance">
+                    Your content optimized for the web.
+                </h1>
+                <div className="flex items-center justify-center gap-3">
+                    <Link href={"/sign-up"}>
+                        <Button className="h-12 w-40 text-base">Sign up</Button>
+                    </Link>
+                    <Link href={"/login"}>
+                        <Button
+                            className="h-12 w-40 text-base"
+                            variant={"outline"}
+                        >
+                            Login
+                        </Button>
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
 }
